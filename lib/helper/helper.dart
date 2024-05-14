32c874e154539eef1b0d8e2f3cb02dee2ab35aa7
KarimTamer <kemotamer448@gmail.com>
2024-05-15 02:32:05 +0300

    modify duration of snakbar

--- lib/helper/helper.dart
+++ lib/helper/helper.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      // width: 100,
+      duration: Duration(seconds: 2),
       backgroundColor: const Color.fromARGB(96, 158, 158, 158),
       content: Center(child: Text(message)),
     ),
